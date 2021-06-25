c9f64860058590756ddd2a5a317dc92b6b3b0412
Hamzoo <awanhamza102@gmail.com>
2021-06-25 19:18:04 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,29 @@ const authentication = require("../app/middlewares/userAuthentication");
 const matchController = require("../app/http/matchController");
 
 router.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 router.get("/admin", authentication.checkAuthenticated, (req, res) => {
-  res.render("admin");
+    res.render("admin");
 });
 router.get("/allMatches", matchController().match, (req, res) => {
-  res.render("allMatches");
+    res.render("allMatches");
 });
 
 router.get("/login", (req, res) => {
-  res.render("login");
+    res.render("login");
 });
 router.post("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/login");
+    req.logout();
+    res.redirect("/login");
 });
-router.get("/register", authentication.checkNotAuthenticated, (req, res) => {
-  res.render("register");
+router.get("/register", (req, res) => {
+    res.render("register");
 });
 
 router.get("/match", (req, res) => {
-  res.render("match");
+    res.render("match");
 });
 
 module.exports = router;
