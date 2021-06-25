90fdbe6a12a52de5d41e01b945575b6b4954a5e7
Hamzoo <awanhamza102@gmail.com>
2021-06-25 19:08:21 +0500

    23

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,67 +6,67 @@ const flash = require("express-flash");
 const passport = require("passport");
 
 /* GET users listing. */
-router.get("/", async (req, res) => {
-  let Users = await UserModel.find();
-  return res.send(Users);
+router.get("/", async(req, res) => {
+    let Users = await UserModel.find();
+    return res.send(Users);
 });
 
 /*Get One User */
-router.get("/:id", async (req, res) => {
-  let User = await UserModel.findById(req.params.id);
-  return res.send(User);
+router.get("/:id", async(req, res) => {
+    let User = await UserModel.findById(req.params.id);
+    return res.send(User);
 });
 
 /*Post New User */
-router.post("/register", async (req, res) => {
-  try {
-    let { name, email, ph_number, password, confirm_password } = req.body;
+router.post("/register", async(req, res) => {
+    try {
+        let { name, email, ph_number, password, confirm_password } = req.body;
 
-    if (!name || !email || !ph_number || !password || !confirm_password) {
-      req.flash("error", "All fields are required");
-      req.flash("name", name);
-      req.flash("email", email);
-      req.flash("ph_number", ph_number);
-      return res.redirect("/register");
+        if (!name || !email || !ph_number || !password || !confirm_password) {
+            req.flash("error", "All fields are required");
+            req.flash("name", name);
+            req.flash("email", email);
+            req.flash("ph_number", ph_number);
+            return res.redirect("/register");
+        }
+
+        let user = await UserModel.findOne({ email });
+
+        if (user) {
+            req.flash("error", "User already registered");
+            return res.redirect("/register");
+        }
+        if (password === confirm_password) {
+            const hashPassword = await bcrypt.hash(password, 10);
+            let User = new UserModel();
+            User.name = name;
+            User.email = email;
+            User.ph_number = ph_number;
+            User.password = hashPassword;
+            User.confirm_password = hashPassword;
+
+            await User.save();
+            return res.redirect("/login");
+        } else {
+            req.flash("error", "Password Not Matched");
+            req.flash("name", name);
+            req.flash("email", email);
+            req.flash("ph_number", ph_number);
+            res.redirect("/register");
+        }
+    } catch (error) {
+        res.status(404).send(error);
     }
-
-    let user = await UserModel.findOne({ email });
-
-    if (user) {
-      req.flash("error", "User already registered");
-      return res.redirect("/register");
-    }
-    if (password === confirm_password) {
-      const hashPassword = await bcrypt.hash(password, 10);
-      let User = new UserModel();
-      User.name = name;
-      User.email = email;
-      User.ph_number = ph_number;
-      User.password = hashPassword;
-      User.confirm_password = hashPassword;
-
-      await User.save();
-      //login
-    } else {
-      req.flash("error", "Password Not Matched");
-      req.flash("name", name);
-      req.flash("email", email);
-      req.flash("ph_number", ph_number);
-      res.redirect("/register");
-    }
-  } catch (error) {
-    res.status(404).send(error);
-  }
 });
 
 /*User Authentication & Authorization */
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/admin",
+        failureRedirect: "/login",
+        failureFlash: true,
+    })
 );
 
 module.exports = router;
