431dea14b6152d734b26bdd1380a7c2d9d74c3d6
Hamzoo <awanhamza102@gmail.com>
2021-06-25 18:26:53 +0500

    5

--- app.js
+++ app.js
@@ -20,17 +20,16 @@ require("./db/connection");
 
 //Session Configuration
 app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoDbStore.create({
-      mongoUrl:
-        "mongodb+srv://rehan:rehan@cluster0.qhfay.mongodb.net/shop?retryWrites=true&w=majority",
-      collectionName: "sessions",
-    }),
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24 hours
-  })
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoDbStore.create({
+            mongoUrl: "mongodb+srv://Hamzoo:123@cluster0.6trjl.mongodb.net/psl?retryWrites=true&w=majority",
+            collectionName: "sessions",
+        }),
+        cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24 hours
+    })
 );
 
 //Passport Configuration
@@ -51,15 +50,15 @@ app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", views_path);
 hbs.registerPartials(partials_path);
-hbs.registerHelper("json", function (obj) {
-  return JSON.stringify(obj);
+hbs.registerHelper("json", function(obj) {
+    return JSON.stringify(obj);
 });
 hbs.registerHelper("ternary", require("handlebars-helper-ternary"));
-hbs.registerHelper("multiply", function (val1, val2) {
-  return val1 * val2;
+hbs.registerHelper("multiply", function(val1, val2) {
+    return val1 * val2;
 });
-hbs.registerHelper("counter", function (index) {
-  return index + 1;
+hbs.registerHelper("counter", function(index) {
+    return index + 1;
 });
 
 app.use(express.json());
@@ -70,36 +69,36 @@ app.use(cookieParser());
 //global middleware
 
 app.use((req, res, next) => {
-  res.locals.session = req.sessions;
-  next();
+    res.locals.session = req.sessions;
+    next();
 });
 
-app.use(function (req, res, next) {
-  // Permission to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+    // Permission to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // Request headers
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+    // Request methods
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    // Request headers
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
+    // Pass to next layer of middleware
+    next();
 });
-app.use(function (req, res, next) {
-  res.locals.session = req.session;
-  res.locals.user = req.user;
-  next();
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    res.locals.user = req.user;
+    next();
 });
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
@@ -108,22 +107,22 @@ app.use("/api/matches", matchRouter);
 // Add headers
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err,
-  });
+    // render the error page
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err,
+    });
 });
 
 module.exports = app;
--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://rehan:rehan@cluster0.qhfay.mongodb.net/PSL?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: true,
-    }
-  )
-  .then(() => {
-    console.log("Connected To MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error while connecting to Database");
-    console.log(error.message);
-  });
+    .connect(
+        "mongodb+srv://Hamzoo:123@cluster0.6trjl.mongodb.net/psl?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: true,
+        }
+    )
+    .then(() => {
+        console.log("Connected To MongoDB");
+    })
+    .catch((error) => {
+        console.log("Error while connecting to Database");
+        console.log(error.message);
+    });
